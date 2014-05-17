a756040b8af3c5c25ec99e7b47d76df108275ba5
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-05-17 13:55:06 +0200

    completed the excercise

--- exercise.py
+++ exercise.py
@@ -1,12 +1,38 @@
+import csv
+import pprint
+
+class FruitMeta(type):
+    def __new__(cls, name, bases, ns, **kw):
+        name = kw.get("filename") or name
+        ns = kw.get("fields") or ns
+        return type.__new__(cls, name, bases, ns)
+
+    def __init__(cls, name, bases, ns, **kw):
+        type.__init__(cls, name, bases, ns)
+
 def get_fruits(filename):
-    # your code here
-    pass
+    returnval = []
+    with open(filename, newline= '') as csvfile:
+        reader = csv.reader(csvfile, delimiter = '\n')
+        field_names = reader.__next__()[0].split(',')
+        class Fruit(object, metaclass=FruitMeta, filename=filename[:-4], fields = {x : None for x in field_names}):
+            pass
+
+
+        for line in reader:
+            split_line = line[0].split(',')
+            new_fruit = Fruit()
+            for i in range(len(field_names)):
+                new_fruit.__setattr__(field_names[i], float(split_line[i]))
+            returnval.append(new_fruit)
+        return returnval
 
 
 f1 = get_fruits("Orange.csv")
 f2 = get_fruits("Pear.csv")
 
-# both oranges and pears have id's and weight, there are 2 of each
+
+
 assert f1[0].id
 assert f2[0].id
 assert f1[0].weight
@@ -18,7 +44,7 @@ assert f1[0].slices
 assert f1[0].__class__.__name__ == "Orange"
 
 # pears are oblong and have diameter
-assert f2[0].onlongness
+assert f2[0].oblongless
 assert f2[0].diameter
 assert f2[0].__class__.__name__ == "Pear"
 
