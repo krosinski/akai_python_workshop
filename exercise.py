8688f971b52d0c749ccc9ea0f559e6d395716139
Dima Tisnek <dimaqq@gmail.com>
2014-05-17 09:53:31 +0200

    unicode exercise

--- exercise.py
+++ exercise.py
@@ -25,3 +25,13 @@ assert f2[0].__class__.__name__ == "Pear"
 total_weight = sum([f.weight for f in f1 + f2])
 print("total weight", total_weight)
 assert total_weight == 1.035
+
+
+# and now for some unicode
+f3 = get_fruits("Ångström.csv")
+assert sum([f.ilość for f in f3]) == 24.33
+
+
+class Foobar(Ångström):
+    def foo(self):
+        return "fun in %s" % self.구역
